907dc48204fccc0cdbfef54ece089f9a4771baeb
miminf <mariem@vaio.com>
2013-05-04 19:48:39 +0100

    Description partie xml finie reste partie activité

--- TuniFund/src/tn/startupfactory/tunifund/interfaceM/DesciptionActivity.java
+++ TuniFund/src/tn/startupfactory/tunifund/interfaceM/DesciptionActivity.java
@@ -6,13 +6,25 @@ import tn.startupfactory.tunifund.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class DesciptionActivity extends Activity {
+	
+	TextView categorie,funded,pledged,to_go,required,text_descrip;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.desciption_projet);
+		
+		categorie=(TextView)findViewById(R.id.categorie);
+		funded=(TextView)findViewById(R.id.funded);
+		pledged=(TextView)findViewById(R.id.pledged);
+		to_go=(TextView)findViewById(R.id.to_go);
+		required=(TextView)findViewById(R.id.required);
+		text_descrip=(TextView)findViewById(R.id.text_descrip);
+		
 	}
 
 	@Override
